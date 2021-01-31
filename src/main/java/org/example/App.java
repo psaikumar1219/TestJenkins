e19a94d310c5c6b47076c94f108382e4ee6165e6
psaikumar1219 <78193485+psaikumar1219@users.noreply.github.com>
2021-01-31 09:51:19 +0530

    Add files via upload

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException {
+        try {
+            String filename = "NewExcelFile.xls" ;
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            HSSFSheet sheet = workbook.createSheet("FirstSheet");
+
+            HSSFRow rowhead = sheet.createRow((short)0);
+            rowhead.createCell(0).setCellValue("No.");
+            rowhead.createCell(1).setCellValue("Name");
+            rowhead.createCell(2).setCellValue("Address");
+
+            HSSFRow row = sheet.createRow((short)1);
+            row.createCell(0).setCellValue("1");
+            row.createCell(1).setCellValue("Sai Kumar");
+            row.createCell(2).setCellValue("India");
+
+            FileOutputStream fileOut = new FileOutputStream(filename);
+            workbook.write(fileOut);
+            fileOut.close();
+            workbook.close();
+            System.out.println("Your excel file has been generated!");
+
+        } catch ( Exception ex ) {
+            System.out.println(ex);
+        }
+    }
+}
+
